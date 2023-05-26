aa2aa33050e58db10469f5ad50a0ca6735c4edb5
Maxim Krasnov <mr.krasnovsky@gmail.com>
2023-05-26 21:30:40 +0300

    complited the solution

--- src/main/java/com/epam/mjc/InterfaceCreator.java
+++ src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -1,32 +1,43 @@
 package com.epam.mjc;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 public class InterfaceCreator {
 
     public Predicate<List<String>> isValuesStartWithUpperCase() {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return strings -> strings.stream().allMatch(str -> str.matches("[A-Z][\\w-]*"));
     }
 
     public Consumer<List<Integer>> addEvenValuesAtTheEnd() {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return integers -> {
+            List<Integer> Values = integers.stream()
+                    .filter(value -> value % 2 == 0)
+                    .collect(Collectors.toList());
+            integers.addAll(Values);
+        };
     }
 
     public Supplier<List<String>> filterCollection(List<String> values) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        values.removeIf(value -> !value.matches("^[A-Z].*\\.$") || value.split(" ").length <= 3);
+        return () -> values;
     }
 
     public Function<List<String>, Map<String, Integer>> stringSize() {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return strings -> strings.stream().collect(Collectors.toMap(str -> str, String::length));
     }
 
     public BiFunction<List<Integer>, List<Integer>, List<Integer>> concatList() {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return (integers, integers2) -> {
+            List<Integer> values = new ArrayList<>();
+            values.addAll(integers);
+            values.addAll(integers2);
+            return values;
+        };
     }
 }
